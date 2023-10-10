8732c4acd1b15582455ef104ca2070a352821c7a
rky-cse <145208505+rky-cse@users.noreply.github.com>
2023-10-10 16:48:57 +0530

    Update script.js (+-)functionalities added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridItems = document.querySelectorAll('.grid-item');
 let ans=0;
-let tot=0
+var tot=0
+let sign =1;
 
 for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener('click', () => {
@@ -14,11 +15,13 @@ for (let i = 0; i < gridItems.length; i++) {
         
         document.getElementById('input').append(i+1);
         ans=(ans*10)+i+1;
+        
        }
        else if((i+1)%4!=0 && i<8){
         
         document.getElementById('input').append(i);
         ans=(ans*10)+i;
+        
        }
        else if((i+1)%4!=0 && i<12){
         
@@ -33,26 +36,33 @@ for (let i = 0; i < gridItems.length; i++) {
        }
        if(i==14){
         document.getElementById('input').append('+');
-        tot=tot+ans;
+        tot+=(sign*ans);
+        sign=1;
         ans=0;
 
        }
        if(i==15){
         document.getElementById('input').append('-');
-        tot-=ans;
-        ans=0;
+     
+        
+    tot+=(sign*ans);
+    sign=-1;
+    ans=0;
+       
 
        }
 
         
     });
-    tot=ans;
+   
    
 
 }
+console.log(tot);
+
 document.getElementById('output').addEventListener("click",myf);
 function myf(){
-    document.getElementById('output').append(tot+ans);
+    document.getElementById('output').append(tot+=(sign*ans));
     console.log(tot);
 
 }

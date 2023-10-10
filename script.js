fac0103ac5b7e35d153daaf7fcc3e6aade752c4c
rky-cse <145208505+rky-cse@users.noreply.github.com>
2023-10-10 17:30:31 +0530

    Update script.js now will work for decimal too

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const gridItems = document.querySelectorAll('.grid-item');
 let ans=0;
 var tot=0
 let sign =1;
+let decimal =false;
+let dc=0;
 
 for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener('click', () => {
@@ -14,42 +16,66 @@ for (let i = 0; i < gridItems.length; i++) {
        if((i+1)%4!=0 && i<4){
         
         document.getElementById('input').append(i+1);
-        ans=(ans*10)+i+1;
+        if(decimal){
+            dc++;
+            ans+=(i+1)*Math.pow(0.1,dc);
+        }
+        else ans=(ans*10)+i+1;
         
        }
        else if((i+1)%4!=0 && i<8){
         
         document.getElementById('input').append(i);
+        if(decimal){
+            dc++;
+            ans+=(i)*Math.pow(0.1,dc);
+        }
+        else
         ans=(ans*10)+i;
         
        }
        else if((i+1)%4!=0 && i<12){
         
         document.getElementById('input').append(i-1);
+        if(decimal){
+            dc++;
+            ans+=(i-1)*Math.pow(0.1,dc);
+        }
+        else
         ans=(ans*10)+i-1;
        }
-       if(i==12){
+       if(i===12){
         document.getElementById('input').append(0);
+        if(decimal){
+            dc++;
+            ans+=(0)*Math.pow(0.1,dc);
+        }
+        else
         ans=(ans*10)+0;
 
        }
-       if(i==13){
+       if(i===13){
         document.getElementById('input').append('.');
+        decimal=true;
        }
-       if(i==14){
+       if(i===14){
         document.getElementById('input').append('+');
         tot+=(sign*ans);
         sign=1;
         ans=0;
+        decimal=false;
+        dc=0;
 
        }
-       if(i==15){
+       if(i===15){
         document.getElementById('input').append('-');
      
         
     tot+=(sign*ans);
     sign=-1;
     ans=0;
+    decimal=false;
+    dc=0;
        
 
        }
